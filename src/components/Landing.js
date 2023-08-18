da7885cdf061977449163206372f099294831373
papai004 <singhapapai004@gmail.com>
2023-08-18 21:43:29 +0530

    added Navbar, landing page

---
+++ src/components/Landing.js
@@ -0,0 +1,11 @@
+import '../styles/landing.css';
+import book_keeper from './book_keeper.png';
+function Landing() {
+  return (
+    <div>
+      <img className='landing_img' src={book_keeper} alt="Home" />
+      <button className="enter_btn"><bold>ENTER</bold></button>
+    </div>
+  );
+}
+export default Landing;
---
+++ src/components/navs/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Nav from './Navbar.module.css';
+import book_keeper from '../book_keeper.png';
+
+const Navbar = () => {
+  return (
+    <div className={Nav.navbar}>
+      <div className={Nav.nav_item}><img className={Nav.nav_img} src={book_keeper} alt="Home" /></div>
+      <div className={Nav.nav_item}>Home</div>
+      <div className={Nav.nav_item}>Books</div>
+      <div className={Nav.nav_item}>Categories</div>
+      <div className={Nav.nav_item}>Authors</div>
+      <div className={Nav.nav_item}>Signin/Signup</div>
+    </div>
+  );
+};
+
+export default Navbar;
