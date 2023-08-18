69644ec9c25d6c843ed4496820059e43e5ede285
papai004 <singhapapai004@gmail.com>
2023-08-18 22:17:57 +0530

    added Navbar, Landing page and some css

--- src/components/navs/Navbar.js
+++ src/components/navs/Navbar.js
@@ -5,7 +5,7 @@ import book_keeper from '../book_keeper.png';
 const Navbar = () => {
   return (
     <div className={Nav.navbar}>
-      <div className={Nav.nav_item}><img className={Nav.nav_img} src={book_keeper} alt="Home" /></div>
+      <div className={Nav.nav_item}><img className={Nav.nav_img} src={book_keeper} alt="Home"/></div>
       <div className={Nav.nav_item}>Home</div>
       <div className={Nav.nav_item}>Books</div>
       <div className={Nav.nav_item}>Categories</div>
