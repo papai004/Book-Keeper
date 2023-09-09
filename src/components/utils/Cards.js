5f4cba4bab6c909d5315c5ee897c6cdea2d2a8dd
papai004 <singhapapai004@gmail.com>
2023-09-09 19:13:14 +0530

    removed some insignificant files

--- src/components/utils/Cards.js
+++ src/components/utils/Cards.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-// import Buttons from "./Buttons";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -23,7 +22,6 @@ function Cards({ booksData }) {
               <Card.Body>
                 <Card.Title>{item.title}</Card.Title>
                 <Card.Text>{item.description}</Card.Text>
-                {/* <Buttons/> */}
               </Card.Body>
               <Button className={cardStyle.btnStyle} variant="dark" onClick={() =>{
                   window.open(item.reading, '_blank');
