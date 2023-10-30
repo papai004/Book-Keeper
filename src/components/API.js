c5d43c4fe5bc2f961b74251e0ab51584fe86ed10
papai004 <singhapapai004@gmail.com>
2023-10-31 00:22:27 +0530

    updated api

--- src/components/API.js
+++ src/components/API.js
@@ -1,9 +1,9 @@
 export const API_URL = 'https://example-data.draftbit.com/books?_limit=80';
 export const BOOK_DETAILS_URL = 'https://example-data.draftbit.com/books/';
-export const MY_COLLECTIONS_URL = 'https://book-keeper1-default-rtdb.firebaseio.com/datas.json';
-export const MY_REVIEWS_URL = 'https://book-keeper1-default-rtdb.firebaseio.com/reviews.json';
-export const CONTACTS_URL = 'https://book-keeper1-default-rtdb.firebaseio.com/contacts.json'
-export const USER_DATA = 'https://book-keeper1-default-rtdb.firebaseio.com/user.json'
+export const MY_COLLECTIONS_URL = 'https://book-keeper-nov-default-rtdb.asia-southeast1.firebasedatabase.app/datas.json';
+export const MY_REVIEWS_URL = 'https://book-keeper-nov-default-rtdb.asia-southeast1.firebasedatabase.app/reviews.json';
+export const CONTACTS_URL = 'https://book-keeper-nov-default-rtdb.asia-southeast1.firebasedatabase.app/contacts.json'
+export const USER_DATA = 'https://book-keeper-nov-default-rtdb.asia-southeast1.firebasedatabase.app/user.json'
 
 
   
